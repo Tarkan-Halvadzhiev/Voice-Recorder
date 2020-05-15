48ccec6f53ccbe2da92da43e24706ca1af71a004
Tahalvadzh <tarkan1998@abv.bg>
2020-05-15 17:38:54 +0300

    Implemented functionality for getting all voice records.

---
+++ app/src/main/java/com/cisco/voicerecorder/utils/RecordFiles.kt
@@ -0,0 +1,26 @@
+package com.cisco.voicerecorder.utils
+
+import java.io.File
+
+
+class RecordFiles {
+    private val directory: String =
+        android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_MUSIC).path
+    private var files: MutableList<File>? = null
+
+    fun getAllRecords(): List<File>? {
+        val directoryFiles: Array<File> = File(directory).listFiles() ?: return emptyList()
+
+        files = mutableListOf()
+        for (file in directoryFiles) {
+            val fileName = file.name
+            if (file.isFile && fileName.contains(".")) {
+                if (file.isFile && fileName.startsWith("record-") && fileName.endsWith(".mp3")) {
+                    files!!.add(file)
+                }
+            }
+        }
+
+        return files
+    }
+}
